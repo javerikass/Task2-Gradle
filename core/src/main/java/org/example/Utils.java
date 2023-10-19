b25f5f9b89b3bb93efa3037557adf215d4b61009
javerikass <javerikass@gmail.com>
2023-10-19 13:06:48 +0300

    feature: add Utils.java

---
+++ core/src/main/java/org/example/Utils.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Utils {
+
+  public static boolean isAllPositiveNumbers(String... str) {
+    for (String s : str) {
+      if (!StringUtils.isPositiveNumber(s)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
